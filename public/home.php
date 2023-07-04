168f3aa2f0fb308685b393ee27eecf749fa2ca48
AgustiantoBlade <agustianto@itbss.ac.id>
2023-07-05 00:37:55 +0700

    perubahan terbaru

--- public/home.php
+++ public/home.php
@@ -6,24 +6,51 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>to do list</title>
-    <link rel="stylesheet" href="style/home.css">
+    <link rel="stylesheet" href="../style/home.css">
 </head>
 
 <body>
-    <header>
-        <div class="profil">
-            <div class="img">
-                <img src="assets/img/Rectangle1.png" alt="">
+    <div class="wraper">
+        <div class="kontenKiri">
+            <div class="profil">
+                <div class="img">
+                    <img src="../assets/img/Rectangle1.png" alt="">
+                </div>
+                <div class="uname">
+                    <h2>Code Blaze</h2>
+                    <p>codeblaze@gmail.com</p>
+                </div>
             </div>
-            <div class="uname">
-                <h2>Code Blaze</h2>
-                <p>codeblaze@gmail.com</p>
+            <div class="imgBig">
+                <img src="../assets/img/Group3981.png" alt="">
+            </div>
+            <div class="loadingBar">
+                <h1>Rocky</h1>
+                <div class="loading">
+                    <div class="loading1"></div>
+                </div>
+                <div class="wrapLoad">
+                    <p>30 Task Completed</p>
+                    <p>30%</p>
+                </div>
             </div>
         </div>
-        <div class="imgBig">
-            <img src="assets/img/Group3981.png" alt="">
+        <div class="kontenKanan">
+            <div class="headWrap">
+                <div class="headButton">
+                    <div class="head">
+                        Your Task
+                    </div>
+                    <button> + </button>
+                </div>
+                <div class="list">
+                    <img src="../assets/img/Category.png" alt="">
+                    <p>Membuat to do list</p>
+                    <div class="date">Jul 5</div>
+                </div>  
+            </div>
+
         </div>
-    </header>
 </body>
 
 </html>
